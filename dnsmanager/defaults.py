13a8bc571bff068ba64d6842b20e60df91a6f530
Andrew Cutler <macropin@gmail.com>
2015-08-14 14:36:41 +1000

    Use hostmaster as SOA

--- dnsmanager/defaults.py
+++ dnsmanager/defaults.py
@@ -5,7 +5,7 @@ ZONE_DEFAULTS_DEFAULT['retry'] = 7200       # 2 hours
 ZONE_DEFAULTS_DEFAULT['expire'] = 2419200   # 1 month
 ZONE_DEFAULTS_DEFAULT['minimum'] = 600      # 10 minutes
 ZONE_DEFAULTS_DEFAULT['ttl'] = 3600         # 1 hour
-ZONE_DEFAULTS_DEFAULT['soa'] = 'dns-admin'
+ZONE_DEFAULTS_DEFAULT['soa'] = 'hostmaster'
 
 DNS_MANAGER_RECIPES_DEFAULT = (
     ('dnsmanager.recipes.GoogleApps', 'Set Google Apps MX / CNAME'),
